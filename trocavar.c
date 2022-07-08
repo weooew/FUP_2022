80760ffe6720fe284185f65d1c5276fa0d184266
weooew <102996679+weooew@users.noreply.github.com>
2022-07-08 20:54:18 -0300

    Create trocavar.c

---
+++ trocavar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a = 0;
+    int b = 0;
+    scanf("%d %d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("%d %d",a, b);
+    return 0;
+}
