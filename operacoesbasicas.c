9ab0b06aac8721a65db9833193af107d9a3b240e
weooew <102996679+weooew@users.noreply.github.com>
2022-07-08 20:55:29 -0300

    Create operacoesbasicas.c

---
+++ operacoesbasicas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a = 0;
+    int b = 0;
+    scanf("%d\n %d", &a, &b);
+    	printf("%d\n", a+b);
+    	printf("%d\n", a-b);
+    	printf("%d\n", a*b);
+    	printf("%d\n", a%b);
+    return 0;
+}
