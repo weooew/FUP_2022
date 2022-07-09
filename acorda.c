12e9a3bf87d5a8aec70dfa8787370e65344b8dc1
weooew <102996679+weooew@users.noreply.github.com>
2022-07-09 12:45:40 -0300

    Update acorda.c

--- acorda.c
+++ acorda.c
@@ -24,3 +24,20 @@ Saida: Se for uma letra maiúscula, imprima a letra minúscula.
     return 0;
 }
 ```
+```c
+#include<stdio.h>
+
+int main(){
+
+    int n;
+
+    do{
+        printf("Digite um número entre 0 e 10: ");
+        scanf("%d", &n);
+    }while(n < 0 || n > 10);
+
+    printf("Valor de n: %d\n", n);
+
+    return 0;
+}
+```
