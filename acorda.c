dcf11aade8a68c3659009937cde1fdb06638d12a
weooew <102996679+weooew@users.noreply.github.com>
2022-07-09 12:49:05 -0300

    Update acorda.c

--- acorda.c
+++ acorda.c
@@ -1,30 +1,4 @@
-```c
-#include <stdio.h>
-int main(){
-/* Entrada: um caracter
-Saida: Se for uma letra maiúscula, imprima a letra minúscula.
-       Se for uma letra minúscula, imprima a letra maiúscula.
-       Se ele for qualquer outro caractere, imprima ele mesmo.*/
-       
-    int x = 0;
-    char letra;
-    x = 'a' -'A';
-    scanf("%c", &letra);
-    //minusculo para maiusculo 
-    if(letra >= 'a' && letra <= 'z'){
-        letra = letra - x;
-    }
-    //maiusculo para minusculo
-    else if(letra >= 'A' && letra <= 'Z'){
-       letra = letra + x; 
-    }
-    //qualquer outro
-    printf("%c", letra);
-    
-    return 0;
-}
-```
-
+## oi
 
 ```c
 #include<stdio.h>
@@ -43,3 +17,4 @@ int main(){
     return 0;
 }
 ```
+
